3ec9de49069c989ce20f5a9ea15bddcb21e2ef91
s044p855 <s044p855@9e6a24f3-9b19-44ae-9697-c6db6dd32426>
2011-09-22 21:47:06 +0000

    git-svn-id: svn+ssh://hpc.quant.ku.edu/projects/svn/repos/simsem@27 9e6a24f3-9b19-44ae-9697-c6db6dd32426

---
+++ trunk/DataGeneration/simsem/R/simDist.R
@@ -0,0 +1,37 @@
+setClass("Runif",
+	representation(
+		Lower="numeric",
+		Upper="numeric"
+	)
+)
+
+setMethod("run", signature="Runif", definition=function(object) 
+		runif(1,object@Lower, object@Upper)
+)
+
+setMethod("summary", signature="Runif", definition=function(object) {
+		print("Random Uniform Distribution Object.")
+		print(paste("Lower bound is ", format(object@Lower, digits=3), ".", sep=""))
+		print(paste("Upper bound is ", format(object@Upper, digits=3), ".", sep=""))
+	}
+)
+
+setClass("Rnorm",
+	representation(
+		Mean="numeric",
+		SD="numeric"
+	)
+)
+
+setMethod("run", signature="Rnorm", definition=function(object) 
+		rnorm(1,object@Mean, object@SD)
+)
+
+setMethod("summary", signature="Rnorm", definition=function(object) {
+		print("Random Normal Distribution Object.")
+		print(paste("Mean is ", format(object@Mean, digits=3), ".", sep=""))
+		print(paste("Standard deviation is ", format(object@SD, digits=3), ".", sep=""))
+	}
+)
+
+setClassUnion("simDist", c("Runif", "Rnorm"))
