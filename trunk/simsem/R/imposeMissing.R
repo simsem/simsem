0667639cabaef0555bc73db0848c53e158146a55
patr1ckm <patr1ckm@9e6a24f3-9b19-44ae-9697-c6db6dd32426>
2012-03-01 22:40:18 +0000

    started testing imposeMissing code
    git-svn-id: svn+ssh://hpc.quant.ku.edu/projects/svn/repos/simsem@197 9e6a24f3-9b19-44ae-9697-c6db6dd32426

--- trunk/simsem/R/imposeMissing.R
+++ trunk/simsem/R/imposeMissing.R
@@ -5,7 +5,7 @@
 testImposeMissing <- function() {
 
   dat1 <- matrix(rep(1,960),ncol=48)
-  datac <- data <- dat1
+
   
   # Imposing Missing with the following arguments produces no missing values
   imposeMissing(data)
@@ -57,11 +57,10 @@ if(!is.null(pmMAR)) {
 
 }
 
-# To improve the missing mechanism, we have 2 parameters instead of just one.
-# Given one arbitrarily distributed covariate, we first set a threshold for values eligible
-# to be missing, and then set a probability of these eligible values to be randomly missing.
-# Basically to percent missing = 
-newMar <- function(data,pm=NULL,cov=NULL,ignoreColumns=NULL,threshold=NULL) {
+
+# Function to make MAR missing based on 1 covariate using the threshold method.
+# ToDo: Extend to multiple covariates
+makeMAR <- function(data,pm=NULL,cov=NULL,ignoreColumns=NULL,threshold=NULL) {
 
   colList <- 1:dim(data)[2]
   misCols <- colList[-c(cov,ignoreColumns)]
@@ -89,58 +88,6 @@ newMar <- function(data,pm=NULL,cov=NULL,ignoreColumns=NULL,threshold=NULL) {
   mismat <- apply(mismat, c(1,2), function(x){if(x && (runif(1) < pr.missing)){x <- TRUE} else {x <- FALSE}})
   return(mismat)
 }
-
-# Function to impose MAR missing based on two covariates
-# Input: raw data, desired percent missing, indices of covariates
-# Output: Logical matrix of values to be deleted
-makeMAR <- function(data,pm=NULL,covs=NULL)
-  {
-    dat <- matrix(FALSE,dim(data)[1],dim(data)[2])
-
-    if(!is.null(pm) && !is.null(covs)) {
-      len.scale <- dim(data)[2]
-
-      mar.pred1 <- covs[1]
-      mar.pred2 <- covs[2]
-
-
-    ## Create a vector of small deviations 1/2 the length of your scales
-      Y <- runif(len.scale*.5,0,.25*pm)
-
-    ## Bind Y with -Y and add pm to each element
-       ## Permute the elements just described to get a vector whose elements will average to pm
-      if((len.scale %% 2)== 0){
-        Z <- sample(c(Y+pm,-Y+pm), size=len.scale, replace=F)
-      } else {
-        Z <- sample(c(Y+pm,-Y+pm,pm), size=len.scale, replace=F)
-      }
-
-    ## fun1 asks the question:
-    ## Is the prob. associated with the current value of some covariate (normally dist. attached to the dataset) <= x
-       ## x = an element of Z (from above)
-      fun1 <- function(x,dat) pnorm(dat,mean(dat),sd(dat)) <= x
-
-    # THIS INDEXING IS GOING TO BE REAL - run it through the interpreter to verify before making changes.
-    # Basically, this divides the items (columns) into 2 groups that don't include the covariates
-      c <- (1:len.scale)[-covs]
-      g1 <- c[1:(length(c)/2)]
-      g2 <- c[((ceiling(length(c)/2) + 1 - length(c)%% 2):length(c))]
-
-
-    ## Apply fun1 to half of the variables using one covariate and to the other half using a different covariate
-       ## Because Z is m elements long and our covariate is p elements long cbind(R1, R2) will total p x m
-      R1 <- sapply(Z[g1],fun1,dat=data[,mar.pred1])
-      R2 <- sapply(Z[g2],fun1,dat=data[,mar.pred2])
-
-      dat[,g1] <- R1
-      dat[,g2] <- R2
-    }
-
-    return(dat)
-
-}
-
-
 
 # Function to make some MCAR missin'
 # Input: Data matrix dimensions, desired percent missing, columns of covariates to not have missingness on
--- trunk/simsem/inst/tests/test_missing.R
+++ trunk/simsem/inst/tests/test_missing.R
@@ -1,10 +1,10 @@
 source("../../R/imposeMissing.R")
 
 # Data Generation: 'C' denotes covariates, 'E' is an even number of columns.
-data <- matrix(rep(rnorm(1000,1,1),19),ncol=19)
-dataC <- cbind(data,rnorm(1000,0,1),rnorm(1000,5,5))
-dataE <- matrix(rep(rnorm(1000,1,1),20),ncol=20)
-dataCE <- cbind(dataE,rnorm(1000,0,1),rnorm(1000,5,5))
+data <- matrix(rnorm(10000,1,1),ncol=20,nrow=10000)
+dataC <- cbind(data,rnorm(10000,0,1))
+dataE <- matrix(rnorm(10000,1,1),ncol=20,nrow=10000)
+dataCE <- cbind(dataE,rnorm(10000,0,1))
 
 # Calls to the function that should generate no missing values
 context("imposeMissing")
@@ -15,22 +15,22 @@ expect_equal(sum(is.na(imposeMissing(data,pmMAR=0))),0)
 expect_equal(sum(is.na(imposeMissing(data,nforms=0))),0)
 
 context("MCAR")
-percentmis <- mean(replicate(100,sum(is.na(imposeMissing(data,pmMCAR=.1)))/length(data)))
+percentmis <- mean(replicate(10,sum(is.na(imposeMissing(data,pmMCAR=.1)))/length(data)))
 expect_true((percentmis > .099) && (percentmis < .101))
 
-percentmis <- mean(replicate(100,sum(is.na(imposeMissing(dataE,pmMCAR=.1)))/length(dataE)))
+percentmis <- mean(replicate(10,sum(is.na(imposeMissing(dataE,pmMCAR=.1)))/length(dataE)))
 expect_true((percentmis > .099) && (percentmis < .101))
 
-percentmis <- mean(replicate(100,sum(is.na(imposeMissing(dataC,covs=c(20,21),pmMCAR=.1)))/length(dataC[,1:19])))
+percentmis <- mean(replicate(10,sum(is.na(imposeMissing(dataC,covs=c(20,21),pmMCAR=.1)))/length(dataC[,1:19])))
 expect_true((percentmis > .099) && (percentmis < .101))
 
 
 context("MAR")
-# percentmis <- mean(replicate(100,sum(is.na(imposeMissing(dataC,covs=c(20,21),pmMAR=.1)))/length(dataC[,1:19])))
-# expect_true((percentmis > .099) && (percentmis < .101))
+percentmis <- mean(replicate(10,sum(is.na(imposeMissing(dataC,covs=c(21,22),pmMAR=.1)))/length(dataC[,1:19])))
+expect_true((percentmis > .099) && (percentmis < .101))
 
-# percentmis <- mean(replicate(100,sum(is.na(imposeMissing(dataCE,covs=c(21,22),pmMAR=.1)))/length(dataCE[,1:20])))
-# expect_true((percentmis > .099) && (percentmis < .101))
+percentmis <- mean(replicate(10,sum(is.na(imposeMissing(dataCE,covs=c(21,22),pmMAR=.1)))/length(dataCE[,1:20])))
+expect_true((percentmis > .099) && (percentmis < .101))
 
 context("planned")
 percentmis <- mean(replicate(10,sum(is.na(imposeMissing(dataE,nforms=3)))/length(dataE)))
@@ -58,5 +58,5 @@ expect_true(sum(is.na(test.dat[,c(1,2,11,12,21,22)])) == 0)
 test.dat <- imposeMissing(dataCE,covs=c(4,8),nforms=3,timePoints=2)
 expect_true(sum(is.na(test.dat[,c(1,2,4,8,13,14)])) == 0)
 
-# percentmis <- mean(replicate(100,sum(is.na(imposeMissing(dataC,covs=c(20,21),pmMCAR=.1,pmMAR=.1,nforms=3)))/length(dataC)))
-# expect_true((percentmis > .37) && (percentmis < .386))
+percentmis <- mean(replicate(10,sum(is.na(imposeMissing(dataC,covs=c(20,21),pmMCAR=.1,pmMAR=.1,nforms=3)))/length(dataC)))
+expect_true((percentmis > .37) && (percentmis < .386))
