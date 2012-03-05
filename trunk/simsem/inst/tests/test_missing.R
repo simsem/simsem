088872035f1511ca4367b37a70cfe244ae8dfbd3
patr1ckm <patr1ckm@9e6a24f3-9b19-44ae-9697-c6db6dd32426>
2012-03-05 17:24:44 +0000

    added new tests
    git-svn-id: svn+ssh://hpc.quant.ku.edu/projects/svn/repos/simsem@208 9e6a24f3-9b19-44ae-9697-c6db6dd32426

--- trunk/simsem/inst/tests/test_missing.R
+++ trunk/simsem/inst/tests/test_missing.R
@@ -1,18 +1,21 @@
 source("../../R/imposeMissing.R")
 
-# Data Generation: 'C' denotes covariates, 'E' is an even number of columns.
+# Data Generation: 'C' denotes covariate, 'E' is an even number of columns.
 data <- matrix(1,ncol=19,nrow=10000)
 dataC <- cbind(data,rnorm(10000,0,1))
 dataE <- matrix(1,ncol=20,nrow=10000)
 dataCE <- cbind(dataE,rnorm(10000,0,1))
+dfC <- as.data.frame(dataC)
 
 # Calls to the function that should generate no missing values
-context("imposeMissing")
+context("No Missing")
 expect_equal(sum(is.na(imposeMissing(data))),0)
 expect_equal(sum(is.na(imposeMissing(data,cov=1))),0)
 expect_equal(sum(is.na(imposeMissing(data,pmMCAR=0))),0)
 expect_equal(sum(is.na(imposeMissing(data,pmMAR=0))),0)
 expect_equal(sum(is.na(imposeMissing(data,nforms=0))),0)
+expect_equal(sum(is.na(imposeMissing(dfC,nforms=0))),0)
+
 
 context("MCAR")
 percentmis <- mean(replicate(10,sum(is.na(imposeMissing(data,pmMCAR=.1)))/length(data)))
@@ -32,7 +35,7 @@ expect_true((percentmis > .099) && (percentmis < .101))
 percentmis <- mean(replicate(10,sum(is.na(imposeMissing(dataCE,cov=21,pmMAR=.1)))/length(dataCE)))
 expect_true((percentmis > .099) && (percentmis < .101))
 
-context("planned")
+context("Planned")
 percentmis <- mean(replicate(10,sum(is.na(imposeMissing(dataE,nforms=3)))/length(dataE)))
 expect_equal(percentmis,.25)
 
@@ -52,11 +55,19 @@ percentmis <- mean(replicate(10,sum(is.na(imposeMissing(dataCE,cov=21,nforms=3,t
                              /length(dataCE[,1:20])))
 expect_true((percentmis > .266) && (percentmis <.267))
 
-# test.dat <- imposeMissing(dataCE,cov=21,nforms=3,timePoints=2)
-# expect_true(sum(is.na(test.dat[,c(1,2,11,12,21,22)])) == 0)
+context("Ignored Variables")
+test.dat <- imposeMissing(dataCE,cov=21,nforms=3,timePoints=2)
+expect_true(sum(is.na(test.dat[,c(1,2,11,12,21)])) == 0)
 
-# test.dat <- imposeMissing(dataCE,cov=,nforms=3,timePoints=2)
-# expect_true(sum(is.na(test.dat[,c(1,2,4,8,13,14)])) == 0)
+test.dat <- imposeMissing(dataCE,cov=21,pmMCAR=.1,ignoreCols=c(9,14))
+expect_true(sum(is.na(test.dat[,c(9,14,21)])) == 0)
+
+test.dat <- imposeMissing(dataCE,cov=21,pmMAR=.1,ignoreCols=c(3,6,9))
+expect_true(sum(is.na(test.dat[,c(3,6,9,21)]))== 0)
+
+test.dat <- imposeMissing(dataCE,cov=21,nforms=3,ignoreCols=5)
+
+context("Additivity")
 
 # percentmis <- mean(replicate(10,sum(is.na(imposeMissing(dataC,covs=c(20,21),pmMCAR=.1,pmMAR=.1,nforms=3)))/length(dataC)))
 # expect_true((percentmis > .37) && (percentmis < .386))
