d9f569227f5e26780684abcad92d2a13568f741d
Miller <patr1ckm@hpc.local>
2012-05-11 12:35:46 -0500

    File re-structuring

--- trunk/simsem/R/findIndTotalVar.R
+++
@@ -1,8 +0,0 @@
-# findIndTotalVar: Find indicator total variances based on loading matrix,
-# total factor covariance, and measurement error variances.
-
-findIndTotalVar <- function(lambda, totalFactorCov, residualVarTheta) {
-    factor.part <- lambda %*% totalFactorCov %*% t(lambda)
-    indicator.var <- diag(factor.part) + residualVarTheta
-    return(as.vector(indicator.var))
-} 
--- trunk/simsem/R/freeVector.R
+++
@@ -1,6 +0,0 @@
-# freeVector: Create a free parameters vector with a starting values in
-# SimVector.c
-
-freeVector <- function(start, ni) {
-    return(new("SimVector", free = rep(NA, ni), param = rep(start, ni)))
-} 
