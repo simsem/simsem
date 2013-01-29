eb545be63499a7c1b17f2df566433fd8dcd93fdd
psunthud <psunthud@gmail.com>
2013-01-29 00:33:43 -0600

    OpenMx work until ex15

--- SupportingDocs/Examples/Version05mx/ex14/ex14.R
+++ SupportingDocs/Examples/Version05mx/ex14/ex14.R
@@ -1,43 +1,38 @@
 library(simsem)
 
-popModel <- "
-f1 =~ 0.8*y1 + 0.8*y2 + 0.8*y3
-f2 =~ 1*y4
-f3 =~ 1*y5
-f1 ~~ 1*f1
-f2 ~~ 1*f2
-f3 ~~ 0.5*f3
-f1 ~~ 0.2*f2
-f3 ~ 0.5*f1 + 0.5*f2
-y1 ~~ 0.36*y1
-y2 ~~ 0.36*y2
-y3 ~~ 0.36*y3
-y4 ~~ 0*y4
-y5 ~~ 0*y5
-"
+Avalues <- matrix(0, 8, 8)
+Avalues[1:3, 6] <- 0.8
+Avalues[4, 7] <- 1
+Avalues[5, 8] <- 1
+Avalues[8, 6] <- 0.5
+Avalues[8, 7] <- 0.5
+Afree <- matrix(FALSE, 8, 8)
+Afree[1:3, 6] <- TRUE
+Afree[8, 6] <- TRUE
+Afree[8, 7] <- TRUE
 
-analyzeModel <- "
-f1 =~ y1 + y2 + y3
-f2 =~ 1*y4
-f3 =~ 1*y5
-f1 ~~ 1*f1
-f2 ~~ f2
-f3 ~~ f3
-f1 ~~ f2
-f3 ~ f1 + f2
-y1 ~~ y1
-y2 ~~ y2
-y3 ~~ y3
-y4 ~~ 0*y4
-y5 ~~ 0*y5
-"
+Svalues <- diag(c(rep(0.36, 3), 0, 0, 1, 1, 0.5))
+Svalues[6, 7] <- Svalues[7, 6] <- 0.2
+Sfree <- matrix(FALSE, 8, 8)
+diag(Sfree)[1:3] <- TRUE
+Sfree[6, 7] <- Sfree[7, 6] <- TRUE
+Fvalues <- cbind(diag(5), matrix(0, 5, 3))
+
+popModel <- mxModel("Model with Single Indicators",
+    type="RAM",
+    mxMatrix(type="Full", nrow=8, ncol=8, values=Avalues, free=Afree, byrow=TRUE, name="A"),
+    mxMatrix(type="Symm", nrow=8, ncol=8, values=Svalues, free=Sfree, byrow=TRUE, name="S"),
+    mxMatrix(type="Full", nrow=5, ncol=8, free=FALSE, values=Fvalues, byrow=TRUE, name="F"),
+    mxMatrix(type="Full", nrow=1, ncol=8, values=rep(0, 8), free=c(rep(TRUE, 5), rep(FALSE, 3)), name="M"),
+    mxRAMObjective("A","S","F","M", dimnames=c(paste0("y", 1:5), "f1", "f2", "f3"))
+)
 
 dist <- c("norm", "norm", "norm", "chisq", "norm")
 n01 <- list(mean=0, sd=1)
 c5 <- list(df=5)
 facDist <- bindDist(dist, n01, n01, n01, c5, n01)
 
-Output <- sim(1000, n=200, analyzeModel, indDist=facDist, generate=popModel, estimator="mlm", lavaanfun="lavaan")
+Output <- sim(1000, n=200, popModel, indDist=facDist)
 getCutoff(Output, 0.05)
 plotCutoff(Output, 0.05)
 summary(Output)
--- SupportingDocs/Examples/Version05mx/ex15/ex15.R
+++ SupportingDocs/Examples/Version05mx/ex15/ex15.R
@@ -1,24 +1,51 @@
 library(simsem)
 library(lavaan)
 
-model <- ' 
-	ind60 =~ x1 + x2 + x3
-	dem60 =~ y1 + a*y2 + b*y3 + c*y4
-	dem65 =~ y5 + a*y6 + b*y7 + c*y8
+Avalues <- matrix(0, 14, 14)
+Avalues[1:3, 12] <- 1
+Avalues[4:7, 13] <- 1
+Avalues[8:11, 14] <- 1
+Avalues[13, 12] <- 0.5
+Avalues[14, 12:13] <- 0.5
+Afree <- matrix(FALSE, 14, 14)
+Afree[2:3, 12] <- TRUE
+Afree[5:7, 13] <- TRUE
+Afree[9:11, 14] <- TRUE
+Afree[13, 12] <- TRUE
+Afree[14, 12:13] <- TRUE
+Alabels <- matrix(NA, 14, 14)
+Alabels[5:7, 13] <- c("a", "b", "c")
+Alabels[9:11, 14] <- c("a", "b", "c")
 
-	dem60 ~ ind60
-	dem65 ~ ind60 + dem60
+Svalues <- diag(c(rep(0.5, 11), 1, 1, 1))
+Svalues[4, 8] <- Svalues[8, 4] <- 0.2
+Svalues[5, 7] <- Svalues[7, 5] <- 0.2
+Svalues[5, 9] <- Svalues[9, 5] <- 0.2
+Svalues[6, 10] <- Svalues[10, 6] <- 0.2
+Svalues[7, 11] <- Svalues[11, 7] <- 0.2
+Svalues[9, 11] <- Svalues[11, 9] <- 0.2
+Sfree <- matrix(FALSE, 14, 14)
+diag(Sfree) <- TRUE
+Sfree[4, 8] <- Sfree[8, 4] <- TRUE
+Sfree[5, 7] <- Sfree[7, 5] <- TRUE
+Sfree[5, 9] <- Sfree[9, 5] <- TRUE
+Sfree[6, 10] <- Sfree[10, 6] <- TRUE
+Sfree[7, 11] <- Sfree[11, 7] <- TRUE
+Sfree[9, 11] <- Sfree[11, 9] <- TRUE
+Fvalues <- cbind(diag(11), matrix(0, 11, 3))
 
-	y1 ~~ y5
-	y2 ~~ y4 + y6
-	y3 ~~ y7
-	y4 ~~ y8
-	y6 ~~ y8
-'
+analyzeModel <- mxModel("PoliticalDemocracy Data",
+    type="RAM",
+    mxMatrix(type="Full", nrow=14, ncol=14, values=Avalues, free=Afree, byrow=TRUE, name="A"),
+    mxMatrix(type="Symm", nrow=14, ncol=14, values=Svalues, free=Sfree, byrow=TRUE, name="S"),
+    mxMatrix(type="Full", nrow=11, ncol=14, free=FALSE, values=Fvalues, byrow=TRUE, name="F"),
+    mxMatrix(type="Full", nrow=1, ncol=14, values=rep(0, 14), free=c(rep(TRUE, 11), rep(FALSE, 3)), name="M"),
+    mxRAMObjective("A","S","F","M", dimnames=c(paste0("x", 1:3), paste0("y", 1:8), "ind60", "dem60", "dem65"))
+)
 
 usedData <- imposeMissing(PoliticalDemocracy, pmMCAR=0.03)
-fit <- sem(model, data=usedData)
+fit <- analyze(analyzeModel, data=usedData)
 
 misstemplate <- miss(logical=is.na(usedData))
-Output <- sim(1000, n=nrow(usedData), model=fit, generate=fit, miss=misstemplate)
+Output <- sim(1000, n=nrow(usedData), model=fit, miss=misstemplate, mxFit=TRUE)
 pValue(fit, Output)
