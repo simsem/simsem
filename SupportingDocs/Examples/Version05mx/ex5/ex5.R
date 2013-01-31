bb370c861681f419e34bb81b9771abc892e1f7b6
unknown <student@PSYC-Quant-4.(none)>
2013-01-31 09:30:06 -0600

    Fix example 5 OpenMx

--- SupportingDocs/Examples/Version05mx/ex5/ex5.R
+++ SupportingDocs/Examples/Version05mx/ex5/ex5.R
@@ -15,20 +15,25 @@ Afree[11, 9] <- TRUE
 Afree[11, 10] <- TRUE
 Alabels <- matrix(NA, 11, 11)
 Alabels[7:8, 11] <- "con1"
+A <- mxMatrix(type="Full", nrow=11, ncol=11, values=Avalues, free=Afree, labels=Alabels, byrow=TRUE, name="A")
 
 Svalues <- diag(c(rep(0.51, 6), rep(0.64, 2), rep(1, 3)))
 Svalues[9, 10] <- Svalues[10, 9] <- 0.5
 Sfree <- matrix(FALSE, 11, 11)
 diag(Sfree)[1:8] <- TRUE
 Sfree[9, 10] <- Sfree[10, 9] <- TRUE
+S <- mxMatrix(type="Symm", nrow=11, ncol=11, values=Svalues, free=Sfree, byrow=TRUE, name="S")
+
 Fvalues <- cbind(diag(8), matrix(0, 8, 3))
+F <- mxMatrix(type="Full", nrow=8, ncol=11, free=FALSE, values=Fvalues, byrow=TRUE, name="F")
+
+Mvalues <- rep(0, 11)
+Mfree <- c(rep(TRUE, 8), rep(FALSE, 3))
+M <- mxMatrix(type="Full", nrow=1, ncol=11, values=Mvalues, free=Mfree, name="M")
 
 popModel <- mxModel("Latent Regression Model",
     type="RAM",
-    mxMatrix(type="Full", nrow=11, ncol=11, values=Avalues, free=Afree, labels=Alabels, byrow=TRUE, name="A"),
-    mxMatrix(type="Symm", nrow=11, ncol=11, values=Svalues, free=Sfree, byrow=TRUE, name="S"),
-    mxMatrix(type="Full", nrow=8, ncol=11, free=FALSE, values=Fvalues, byrow=TRUE, name="F"),
-    mxMatrix(type="Full", nrow=1, ncol=11, values=rep(0, 11), free=c(rep(TRUE, 8), rep(FALSE, 3)), name="M"),
+    A, S, F, M,
     mxRAMObjective("A","S","F","M", dimnames=c(paste0("y", 1:8), "f1", "f2", "f3"))
 )
 
