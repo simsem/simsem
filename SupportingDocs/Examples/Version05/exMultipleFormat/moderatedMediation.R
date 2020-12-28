aa94610de52a0cfea80b25623f1018b603c82ef2
TDJorgensen <TJorgensen314@gmail.com>
2020-12-28 12:44:44 -0500

    update moderatedMediation example

--- SupportingDocs/Examples/Version05/exMultipleFormat/moderatedMediation.R
+++ SupportingDocs/Examples/Version05/exMultipleFormat/moderatedMediation.R
@@ -1,12 +1,12 @@
 ### Terrence D. Jorgensen
-### Last updated: 26 October 2020
+### Last updated: 28 December 2020
 ### example of moderated mediation simulation, in response to this thread:
 ### https://groups.google.com/d/msg/lavaan/otcOcc7Rclw/1pQ22N9dAAAJ
 
 ### 3 (Self-efficacy) x 2 (Feedback condition) design with 5 dummy codes for 6 groups:
 ### X1:  0 = Control, 0 = Low Self-efficacy, 1 = High Self-efficacy
 ### X2: 0 = Control, 1 = Low Self-efficacy, 0 = High Self-efficacy
-### W: 0 = Ambiguous, 1 = Unambiguous 
+### W: 0 = Ambiguous, 1 = Unambiguous
 ### Interaction 1: W*X1
 ### Interaction 2: W*X2
 
@@ -19,11 +19,11 @@ library(simsem)
 ## -----------------------------------------
 
 ## paths when moderator (W) dummy code(s) == 0
-a1.amb <- .3 # effect of X1 when W == 0
+a1.amb <- .1 # effect of X1 when W == 0
 a2.amb <- .3 # effect of X2 when W == 0
 ## how much "a" paths are moderated by W
-a1.unamb <- .3 # effect of (X1 * W) dummy-code interaction
-a2.unamb <- .3 # effect of (X2 * W) dummy-code interaction
+a1.unamb <- -.2 # effect of (X1 * W) dummy-code interaction
+a2.unamb <- -.4 # effect of (X2 * W) dummy-code interaction
 
 ## simple effect of moderator (W) on M or Y when X1 & X2 == 0
 w.M <- 0 # no effect? Doesn't really matter, not the effect of interest
@@ -31,9 +31,9 @@ w.Y <- 0
 
 ## paths unaffected by W (since only X and W interact, and you are
 ##                        only interested in conditional INDIRECT effects)
-b <- .3 # effect of M on Y
-c1.amb <- .3 # direct effect of X1 on Y
-c2.amb <- .3 # direct effect of X2 on Y
+b <- .5 # effect of M on Y
+c1.amb <- .2 # direct effect of X1 on Y
+c2.amb <- .4 # direct effect of X2 on Y
 c1.unamb <- 0 # no moderation of direct effects on Y
 c2.unamb <- 0
 
@@ -57,7 +57,7 @@ designMatrix
 
 ## total sample size must be a multiple of 6 (assuming balanced groups)
 N.per.group <- 10
-N <- 6 * N.per.group
+N <- N.per.group * nrow(designMatrix)
 ## stack N.per.group copies of design matrix for a full sample of size N
 dummies <- designMatrix[c("X1","X2","W","X1W","X2W")]
 covData <- do.call(rbind, lapply(1:N.per.group, function(i) dummies))
@@ -118,6 +118,15 @@ kappa.pop["Y","X1W"] <- c1.unamb
 kappa.pop["Y","X2W"] <- c2.unamb
 
 (exoPaths <- bind(free = kappa.free, popParam = kappa.pop))
+## NOTE:  kappa will get stored as gamma because the LISREL parameterization
+## only allows observed variables (and factors) to be predicted by factors.
+## So below it will be passed to the GA= rather than KA= argument.
+
+
+## Set the intercepts of M and Y to 0. As kappa gets assigned to gamma,
+## TY= would get transferred to AL=
+(AL <- bind(free = c(NA, NA), popParam = c(0, 0)))
+
 
 
 ## specify any indirect or total effects you want power for:
@@ -135,12 +144,10 @@ userParams <- '
 '
 
 
-simMod <- model.path(BE = endoPaths, RPS = residCor, KA = exoPaths,
+simMod <- model.path(BE = endoPaths, RPS = residCor, GA = exoPaths, AL = AL,
                      indLab = c("M","Y"), covLab = names(dummies),
                      con = userParams)
-simMod # kappa gets stored as gamma because the LISREL parameterization
-## only allows observed variables (and factors) to be predicted by factors.
-## The intercepts/means were also arbitrarily set to 0. Does not affect results.
+simMod
 
 
 
@@ -148,6 +155,17 @@ simMod # kappa gets stored as gamma because the LISREL parameterization
 ## Specify parameters matrices
 ## ---------------------------
 
-out <- sim(nRep = 10, n = N, model = simMod, covData = covData)
-summaryParam(out, matchParam = TRUE)
+out <- sim(nRep = 200, n = N, model = simMod, covData = covData)
+summaryParam(out, matchParam = TRUE) #FIXME: average estimates != parameters
+
+## The problem is that exogenous effects are assigned in the wrong order.
+## See the labels in the parameter table:
+data.frame(simMod@pt[c("lhs","op","rhs","label")])[simMod@pt$op == "~",]
+## The "a" labels should all be effects on M, and "c" labels are effects on Y.
+## Instead, the labels (and thus, population parameters) appear to be assigned
+## in the order produced by vectorizing the kappa matrix:
+simMod@dgen$GA@free
+as.character(simMod@dgen$GA@free) # wrong order
+as.numeric(simMod@dgen$GA@popParam) # likewise
+
 
